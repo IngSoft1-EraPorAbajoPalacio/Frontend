6dc262f8632b3d1df748314d367c50ff20c2be49
Maurillugdar <maurillugdar@gmail.com>
2024-09-21 15:34:51 -0300

    ING-52 funcionalidades agregadas

--- src/components/layouts/FormCreateRoom.tsx
+++ src/components/layouts/FormCreateRoom.tsx
@@ -9,29 +9,93 @@ export function FormCreateRoom() {
 		}
 	);
 
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log(form);
+	};
+
+	const handleRoomNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if (1/*checkAppropiateRoomName(e.target.value)*/) {
+			setForm({
+				...form,
+				room: e.target.value,
+			});
+		}
+	};
+
+	const incrementMaxPlayersAllowed = () => {
+		if(form.maxPlayers < 8) {
+			setForm({
+				...form,
+				maxPlayers: form.maxPlayers + 1,
+			});
+		}
+	};
+
+	const decrementMaxPlayersAllowed = () => {
+		if(2 < form.maxPlayers && form.minPlayers < form.maxPlayers) {
+			setForm({
+				...form,
+				maxPlayers: form.maxPlayers - 1,
+			});
+		}
+	};
+
+	const incrementMinPlayersAllowed = () => {
+		if(form.minPlayers < 8 && form.minPlayers < form.maxPlayers) {
+			setForm({
+				...form,
+				minPlayers: form.minPlayers + 1,
+			});
+		}
+	};
+
+	const decrementMinPlayersAllowed = () => {
+		if(2 < form.minPlayers) {
+			setForm({
+				...form,
+				minPlayers: form.minPlayers - 1,
+			});
+		}
+	};
+
 	return (<>
-		<form onSubmit={()=>{}}>
+		<form onSubmit={handleSubmit}>
 			<label>
 				Nombre de la Sala:
 				<input type='text'
 					name='sala'
 					placeholder="SalaDeTorval"
 					value={form.room}
-					onChange={()=>{}}/>
+					onChange={handleRoomNameChange} />
 			</label>
 			<label>
+			<div>
 				Máximo de Jugadores:
-				<input type='number'
-				name='maxJugadores'
-				value={form.maxPlayers}
-				onChange={()=>{}}/>
+					<button type="button"
+						onClick={() => decrementMaxPlayersAllowed()}>
+						<b>-</b>
+					</button>
+					<span>{form.maxPlayers}</span>
+					<button type="button"
+						onClick={() => incrementMaxPlayersAllowed()}>
+						<b>+</b>
+					</button>
+				</div>
 			</label>
 			<label>
 				Mínimo de Jugadores:
-				<input type='number'
-				name='minJugadores'
-				value={form.minPlayers}
-				onChange={()=>{}}/>
+				<div>
+					<button type="button"
+						onClick={decrementMinPlayersAllowed}>
+						<b>-</b>
+					</button>
+					<span>{form.minPlayers}</span>
+					<button type="button"
+						onClick={incrementMinPlayersAllowed}>
+						<b>+</b>
+					</button>
+				</div>
 			</label>
 		</form>
 	</>);
