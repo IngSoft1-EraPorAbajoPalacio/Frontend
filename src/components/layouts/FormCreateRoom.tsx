ffd43d89cfc6624cfbd64e6e58c686d683c4438b
Maurillugdar <maurillugdar@gmail.com>
2024-09-23 14:02:10 -0300

    ING-52 envio de datos en el formulario funcionando, probado con el back

--- src/components/layouts/FormCreateRoom.tsx
+++ src/components/layouts/FormCreateRoom.tsx
@@ -3,16 +3,20 @@ import { useState } from 'react';
 import "../../styles/FormCreateRoom.css";
 
 interface FormInputs {
+	idPlayer: string,
+	playerName: string,
 	room: string,
 	minPlayers: number,
 	maxPlayers: number,
 }
 
 export function FormCreateRoom() {
-	const [dirty, setDirty] = useState<boolean>(false);
+	const [dirty, setDirty] = useState<boolean>(false); // To check if the information of the input is missing
 
 	const [form, setForm] = useState<FormInputs>(
 		{
+			idPlayer: "aaa1",
+			playerName: 'Player',
 			room: '',
 			minPlayers: 2,
 			maxPlayers: 4,
@@ -21,7 +25,32 @@ export function FormCreateRoom() {
 
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		console.log(form);
+		const data = {
+			id_host: form.idPlayer,
+			nombre_host: form.playerName,
+			nombre_partida: form.room,
+			cant_min_jugadores: form.minPlayers,
+			cant_max_jugadores: form.maxPlayers,
+		};
+		const options = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' }, //Ill send a json
+			body: JSON.stringify(data), //Here is the json
+		};
+		const asyncPost = async () => {
+			try {
+				//Now i have to connect with the endpoint to send the info of the room
+				const response = await fetch('http://127.0.0.1:8000/partida', options);
+				if (response.ok) {
+					console.log('Room created');
+				} else {
+					console.log('Failed to create room');
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		}
+		asyncPost();
 	};
 
 	const handleRoomNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
