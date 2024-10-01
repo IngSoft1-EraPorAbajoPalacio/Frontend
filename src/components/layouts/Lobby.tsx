2b0c4d85151ca312b854d1902f2433f41f4fc0a1
JulietaStorino <julieta_storino@mi.unc.edu.ar>
2024-09-30 23:24:36 -0300

    feature_ING-55_redirección_completa

--- src/components/layouts/Lobby.tsx
+++ src/components/layouts/Lobby.tsx
@@ -2,37 +2,38 @@ import { ListarJugadores, iniciarPartida } from "../views/Public/Lobby/MenajesLo
 import { obtenerJugador, obtenerPartida } from "../context/GameContext";
 import { useState } from 'react';
 import '../../styles/Lobby.css';
-import obtenerDatosPartida from "../hooks/ObtenerDatosPartida";
+import Juego from "./Juego";
 
 function Lobby() {
   const partida = obtenerPartida();
   const jugador = obtenerJugador();
   const [jugadores, setJugadores] = useState<{ id: number, nombre: string }[]>([]);
   const [CantidadJugadores, setCantidadJugadores] = useState<number>(1);
+  const [partidaEnCurso, setPartidaEnCurso] = useState(false);
 
-  ListarJugadores(setJugadores, setCantidadJugadores);
+  ListarJugadores(setJugadores, setCantidadJugadores, setPartidaEnCurso);
 
   const handleIniciarPartida = () => {
     iniciarPartida(partida.id, jugador.id);
   };
 
-  obtenerDatosPartida();
-
   return (
-    <div className="lobby-container">
-      <h1 className="lobby-title">{partida.nombre}</h1>
-      <p className="lobby-subtitle">Esperando a jugadores...</p>
-      <ul className="lobby-list">
-        {jugadores.map((jugadorListado) => (
-          <li key={jugadorListado.id} className="lobby-list-item">
-            <p>{jugadorListado.nombre}</p>
-          </li>
-        ))}
-      </ul>
-      {jugador.isHost && CantidadJugadores >= partida.cantJugadoresMin && (
-        <button className="lobby-button" onClick={handleIniciarPartida}>Iniciar Partida</button>
-      )}
-    </div>
+    <>
+      {partidaEnCurso ? <Juego/> :
+        <div className="lobby-container">
+          <h1 className="lobby-title">{partida.nombre}</h1>
+          <p className="lobby-subtitle">Esperando a jugadores...</p>
+          <ul className="lobby-list">
+            {jugadores.map((jugadorListado) => (
+              <li key={jugadorListado.id} className="lobby-list-item"> <p>{jugadorListado.nombre}</p> </li>
+            ))}
+          </ul>
+          {jugador.isHost && CantidadJugadores >= partida.cantJugadoresMin && (
+            <button className="lobby-button" onClick={handleIniciarPartida}>Iniciar Partida</button>
+          )}
+        </div>
+      }
+    </>
   );
 }
 
--- src/components/views/Public/Lobby/MenajesLobby.tsx
+++ src/components/views/Public/Lobby/MenajesLobby.tsx
@@ -1,10 +1,13 @@
 import { socket } from "../../../hooks/sockets";
 import { useEffect } from 'react';
 import axios from "axios"
+import { JugadorEnCurso, PartidaEnCurso } from "../../../../types/partidaEnCurso";
+import { obtenerJugador, obtenerPartida, guardarPartidaEnCurso } from '../../../context/GameContext';
 
 export const ListarJugadores = (
     setJugador: React.Dispatch<React.SetStateAction<{id: number, nombre: string}[]>>,
-    setContador: React.Dispatch<React.SetStateAction<number>>
+    setContador: React.Dispatch<React.SetStateAction<number>>,
+    SetList: React.Dispatch<React.SetStateAction<boolean>>
 ) => {
     useEffect(() => {
         socket.onmessage = (event) => {
@@ -14,9 +17,34 @@ export const ListarJugadores = (
                 setJugador(message.ListaJugadores);
                 setContador(message.ListaJugadores.length);
             }
+            if (message.type === 'IniciarPartida') {
+                SetList(true);
+                handleIniciarPartida(message);
+              }
         };
     }, [setJugador, setContador]);
 };
+
+const handleIniciarPartida = (mensaje: any) => {
+    const jugadores = mensaje.cartasFigura.map( ( mazo: {"idJugador": number, "nombreJugador": string, "cartas":[{"id": number, "figura": number}]} ) => {
+      const jugador = (mazo.idJugador === obtenerJugador().id) ?
+        new JugadorEnCurso(mazo.idJugador, mazo.nombreJugador, mazo.cartas, mensaje.cartasMovimiento, true, true) :
+        new JugadorEnCurso(mazo.idJugador, mazo.nombreJugador, mazo.cartas, [], true, false);
+      return jugador;
+    });
+
+    // Crear una nueva instancia de PartidaEnCurso
+    const partida = new PartidaEnCurso(
+      obtenerPartida().id, 
+      obtenerPartida().nombre, 
+      mensaje.orden.length, 
+      jugadores, 
+      mensaje.fichas, 
+      mensaje.orden
+    );
+    guardarPartidaEnCurso(partida); // Guardar la partida en el contexto
+
+  };
 
 export const iniciarPartida = async (idPartida:number, idJugador: number) => {
     try {
