0f12057b2c3f2b3cc9e27cdd2d6fc6fbc3fccc79
JulietaStorino <julieta_storino@mi.unc.edu.ar>
2024-10-02 11:26:31 -0300

    feature_ING-45_boton_iniciar_partida_y_WS_corregidos

--- src/components/layouts/Lobby.tsx
+++ src/components/layouts/Lobby.tsx
@@ -1,43 +1,46 @@
-import { ListarJugadores, iniciarPartida } from "../views/Public/Lobby/MenajesLobby";
-import { obtenerJugador, obtenerPartida, obtenerJugadoresUnidos } from "../context/GameContext";
+import { ListarJugadores, iniciarPartida } from '../views/Public/Lobby/MenajesLobby';
+import { obtenerJugador, obtenerPartida, obtenerJugadoresUnidos } from '../context/GameContext';
 import { useState, useEffect } from 'react';
+import { Jugador , Partida } from '../../types/partidaListada';
 import '../../styles/Lobby.css';
-import Juego from "./Juego";
+import Juego from './Juego';
 
 function Lobby() {
-  const partida = obtenerPartida();
-  const jugador = obtenerJugador();
   const jugadoresUnidos = obtenerJugadoresUnidos();
   const [jugadores, setJugadores] = useState<{ id: number, nombre: string }[]>(jugadoresUnidos);
   const [CantidadJugadores, setCantidadJugadores] = useState<number>(1);
   const [partidaEnCurso, setPartidaEnCurso] = useState(false);
-  const [Host, setHost] = useState<boolean>(false);
+  const [jugador, setJugador] = useState<Jugador>();
+  const [partida, setPartida] = useState<Partida>();
 
   ListarJugadores(setJugadores, setCantidadJugadores, setPartidaEnCurso);
 
   const handleIniciarPartida = () => {
-    iniciarPartida(partida.id, jugador.id);
+    if (partida && jugador) {
+      iniciarPartida(partida.id, jugador.id);
+    }
   };
 
 
   useEffect(() => {
-    setHost(jugador.isHost);
+    setJugador(obtenerJugador());
+    setPartida(obtenerPartida());
   }, []);
 
   return (
     <>
       {partidaEnCurso ? <Juego/> :
-        <div className="lobby-container">
-          <h1 className="lobby-title">{partida.nombre}</h1>
-          <p className="lobby-subtitle">Esperando a jugadores...</p>
-          <ul className="lobby-list">
+        <div className='lobby-container'>
+          {partida && <h1 className='lobby-title'>{partida.nombre}</h1>}
+          <p className='lobby-subtitle'>Esperando a jugadores...</p>
+          <ul className='lobby-list'>
             {jugadores.map((jugadorListado) => (
-              <li key={jugadorListado.id} className="lobby-list-item"> <p>{jugadorListado.nombre}</p> </li>
+              <li key={jugadorListado.id} className='lobby-list-item'> <p>{jugadorListado.nombre}</p> </li>
             ))}
           </ul>
 
-          {Host && CantidadJugadores >= partida.cantJugadoresMin && (
-            <button className="lobby-button" onClick={handleIniciarPartida}>Iniciar Partida</button>
+          {partida && jugador && jugador.isHost && CantidadJugadores >= partida.cantJugadoresMin && (
+            <button className='lobby-button' onClick={handleIniciarPartida}>Iniciar Partida</button>
           )}
         </div>
       }
--- src/components/views/Public/Lobby/MenajesLobby.tsx
+++ src/components/views/Public/Lobby/MenajesLobby.tsx
@@ -4,49 +4,60 @@ import axios from "axios"
 import { JugadorEnCurso, PartidaEnCurso } from "../../../../types/partidaEnCurso";
 import { obtenerJugador, obtenerPartida, guardarPartidaEnCurso, borrarJugadoresUnidos } from '../../../context/GameContext';
 
+// Escucha los mensajes del servidor en el lobby
 export const ListarJugadores = (
-    setJugador: React.Dispatch<React.SetStateAction<{id: number, nombre: string}[]>>,
-    setContador: React.Dispatch<React.SetStateAction<number>>,
-    SetList: React.Dispatch<React.SetStateAction<boolean>>
+  setJugador: React.Dispatch<React.SetStateAction<{id: number, nombre: string}[]>>,
+  setContador: React.Dispatch<React.SetStateAction<number>>,
+  SetList: React.Dispatch<React.SetStateAction<boolean>>
 ) => {
-    useEffect(() => {
-        socket.onmessage = (event) => {
-            console.log(event);
-            const message = JSON.parse(event.data);
-            if (message.type === 'JugadorUnido') {
-                setJugador(message.ListaJugadores);
-                setContador(message.ListaJugadores.length);
-                borrarJugadoresUnidos();
-            }
-            if (message.type === 'IniciarPartida') {
-                SetList(true);
-                handleIniciarPartida(message);
-              }
-        };
-    }, [setJugador, setContador]);
+  socket.onmessage = (event) => {
+    console.log(event);
+    const message = JSON.parse(event.data);
+    // Si el mensaje es de tipo JugadorUnido, actualiza la lista de jugadores en el lobby
+    if (message.type === 'JugadorUnido') {
+      setJugador(message.ListaJugadores);
+      setContador(message.ListaJugadores.length);
+      borrarJugadoresUnidos();
+    }
+    // Si el mensaje es de tipo IniciarPartida, llama a la API para inicia la partida
+    if (message.type === 'IniciarPartida') {
+      SetList(true);
+      handleIniciarPartida(message);
+    }
+  };
 };
 
 const handleIniciarPartida = (mensaje: any) => {
-    const jugadores = mensaje.cartasFigura.map( ( mazo: {"idJugador": number, "nombreJugador": string, "cartas":[{"id": number, "figura": number}]} ) => {
-      const jugador = (mazo.idJugador === obtenerJugador().id) ?
-        new JugadorEnCurso(mazo.idJugador, mazo.nombreJugador, mazo.cartas, mensaje.cartasMovimiento, true, true) :
-        new JugadorEnCurso(mazo.idJugador, mazo.nombreJugador, mazo.cartas, [], true, false);
-      return jugador;
-    });
 
-    // Crear una nueva instancia de PartidaEnCurso
-    const partida = new PartidaEnCurso(
-      obtenerPartida().id, 
-      obtenerPartida().nombre, 
-      mensaje.orden.length, 
-      jugadores, 
-      mensaje.fichas, 
-      mensaje.orden
-    );
-    guardarPartidaEnCurso(partida); // Guardar la partida en el contexto
+  // Obtener el id del jugador del contexto
+  const idJugador = obtenerJugador().id;
 
-  };
+  // Filtrar las cartas de movimiento del jugador
+  const movimientos = mensaje.cartasMovimiento.filter(
+    (mazo: {"idJugador": number, "nombreJugador": string, "cartas": [{"id": number, "movimiento": number}]}) => mazo.idJugador === idJugador);
+  
+  // Crea una nueva instancia de cada jugador de la partida
+  const jugadores = mensaje.cartasFigura.map( ( mazo: {"idJugador": number, "nombreJugador": string, "cartas":[{"id": number, "figura": number}]} ) => {
+    const jugador = (mazo.idJugador === idJugador) ?
+      new JugadorEnCurso(mazo.idJugador, mazo.nombreJugador, mazo.cartas, movimientos, true, true) :
+      new JugadorEnCurso(mazo.idJugador, mazo.nombreJugador, mazo.cartas, [], true, false);
+    return jugador;
+  });
 
+  // Crear una nueva instancia de PartidaEnCurso
+  const partida = new PartidaEnCurso(
+    obtenerPartida().id, 
+    obtenerPartida().nombre, 
+    mensaje.orden.length, 
+    jugadores, 
+    mensaje.fichas, 
+    mensaje.orden
+  );
+  guardarPartidaEnCurso(partida); // Guardar la partida en el contexto
+
+};
+
+// Llama a la API para iniciar la partida
 export const iniciarPartida = async (idPartida:number, idJugador: number) => {
     try {
       const url = `http://localhost:8000/partida/${idPartida}/jugador/${idJugador}`;
