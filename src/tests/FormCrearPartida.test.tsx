6643b4b8ee0d8f62888552a4b00b54da15cc4d18
Maurillugdar <maurillugdar@gmail.com>
2024-10-08 00:54:48 -0300

    ING-98 Tests para formulario de unirse y crear partida terminados

--- src/tests/FormCrearPartida.test.tsx
+++ src/tests/FormCrearPartida.test.tsx
@@ -2,41 +2,79 @@ import FormCreateRoom from "../components/views/Public/Home/FormularioCrearParti
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { vi } from 'vitest';
+import Home from "../components/layouts/Home";
+import { BrowserRouter, useNavigate } from "react-router-dom";
+import Lobby from "../components/layouts/Lobby";
 
-const mockedSets = vi.fn(); 
+
+const mockedSets = vi.fn();
 const mockMaxLonNombres = 20;
 
-test('render formulario de creacion', () => {
-    render(<FormCreateRoom setIdPartida={mockedSets} setIdJugador={mockedSets} />);
-    const existe = screen.getByText(/Nombre de partida:/i); // Veo que se muestre la frase/titulo del form de creacion
-    expect(existe).toBeVisible();
+const MockCreateRoomForm = () => {
+    return <FormCreateRoom setIdPartida={mockedSets} setIdJugador={mockedSets} />;
+};
+
+
+describe('Unitest', () => {
+
+    test('render formulario de creacion', () => {
+        render(<MockCreateRoomForm />);
+        const existe = screen.getByText(/Nombre de partida:/i); // Veo que se muestre la frase/titulo del form de creacion
+        expect(existe).toBeVisible();
+    });
+
+    test('interactuar con input de nombre de partida', () => {
+        render(<MockCreateRoomForm />);
+        const nombrePartida = screen.getByPlaceholderText("SalaDeTorval") as HTMLInputElement;
+        fireEvent.change(nombrePartida, { target: { value: "EraPorAbajoPalacio" } });
+        expect(nombrePartida.value).toBe("EraPorAbajoPalacio");
+    });
+
+    test('interactuar con input de alias jugador', () => {
+        render(<MockCreateRoomForm />);
+        const alias = screen.getByPlaceholderText("Player1") as HTMLInputElement;
+        fireEvent.change(alias, { target: { value: "Torval" } });
+        expect(alias.value).toBe("Torval");
+    });
+
+    test('limite de cantidad de letras alias jugador', () => {
+        render(<MockCreateRoomForm />);
+        const alias = screen.getByPlaceholderText("Player1") as HTMLInputElement;
+        fireEvent.change(alias, { target: { value: "abdulajrabinachbinbar" } }); //21 caracteres
+        expect(alias.value.length).not.toBeGreaterThan(mockMaxLonNombres);
+    });
+
+    test('limite de cantidad de letras nombre partida', () => {
+        render(<MockCreateRoomForm />);
+        const nombrePartida = screen.getByPlaceholderText("SalaDeTorval") as HTMLInputElement;
+        fireEvent.change(nombrePartida, { target: { value: "abdulajrabinachbinbar" } }); //21 caracteres
+        expect(nombrePartida.value.length).not.toBeGreaterThan(mockMaxLonNombres);
+    });
+
+    test('no submit sin nombre de Partida, ni alias', () => {
+        render(<MockCreateRoomForm />);
+        const nombrePartida = screen.getByPlaceholderText("SalaDeTorval");
+        const alias = screen.getByPlaceholderText("Player1") as HTMLInputElement;
+        expect(alias).toBeRequired();
+        expect(nombrePartida).toBeRequired();
+    });
+
 });
 
-test('interactuar con input de nombre de partida', () => {
-    render(<FormCreateRoom setIdPartida={mockedSets} setIdJugador={mockedSets} />);
-    const nombrePartida =  screen.getByPlaceholderText("SalaDeTorval") as HTMLInputElement;
-    fireEvent.change(nombrePartida, {target: {value: "EraPorAbajoPalacio"}});
-    expect(nombrePartida.value).toBe("EraPorAbajoPalacio");
-});
 
-test('interactuar con input de alias jugador', () => {
-    render(<FormCreateRoom setIdPartida={mockedSets} setIdJugador={mockedSets} />);
-    const alias =  screen.getByPlaceholderText("Player1") as HTMLInputElement;
-    fireEvent.change(alias, {target: {value: "Torval"}});
-    expect(alias.value).toBe("Torval");
-});
+describe('Integration Test', () => {
 
-test('limite de cantidad de letras alias jugador', () => {
-    render(<FormCreateRoom setIdPartida={mockedSets} setIdJugador={mockedSets} />);
-    const alias =  screen.getByPlaceholderText("Player1") as HTMLInputElement;
-    fireEvent.change(alias, {target: {value: "abdulajrabinachbinbar"}}); //21 caracteres
-    expect(alias.value.length).not.toBeGreaterThan(mockMaxLonNombres);
-});
+    const MockHome = () => {
+        return <BrowserRouter><Home /></BrowserRouter>;
+    };
 
-test('limite de cantidad de letras nombre partida', () => {
-    render(<FormCreateRoom setIdPartida={mockedSets} setIdJugador={mockedSets} />);
-    const nombrePartida =  screen.getByPlaceholderText("SalaDeTorval") as HTMLInputElement;
-    fireEvent.change(nombrePartida, {target: {value: "abdulajrabinachbinbar"}}); //21 caracteres
-    expect(nombrePartida.value.length).not.toBeGreaterThan(mockMaxLonNombres);
-});
+    test('click abrir form de crear partida', () => {
+        render(<MockHome />);
+        const botonAbrirFormC = screen.getByText(/Crear partida/i);
+        fireEvent.click(botonAbrirFormC);
+        const existe = screen.getByText(/Nombre de partida:/i);
+        expect(existe).toBeVisible();
+    })
 
+
+});
--- src/tests/FormUnirsePartida.test.tsx
+++ src/tests/FormUnirsePartida.test.tsx
@@ -2,27 +2,39 @@ import FormularioUnirsePartida from "../components/views/Public/Home/FormularioU
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { vi } from 'vitest';
+import { BrowserRouter } from "react-router-dom";
+import Home from "../components/layouts/Home";
 
-const mockedSets = vi.fn(); 
+const mockedSets = vi.fn();
 const mockMaxLonNombres = 20;
 const mockIdPartida = 0;
 
+const MockJoinRoom = () => {
+    return <FormularioUnirsePartida idPartida={mockIdPartida} setIdJugador={mockedSets} />;
+};
+
 test('render formulario de unirse', () => {
-    render(<FormularioUnirsePartida idPartida={mockIdPartida} setIdJugador={mockedSets}/>);
+    render(<MockJoinRoom/>);
     const existe = screen.getByText(/Unirse a Sala:/i); // Veo que se muestre la frase/titulo del form de creacion
     expect(existe).toBeVisible();
 });
 
 test('interactuar con input de alias jugador', () => {
-    render(<FormularioUnirsePartida idPartida={mockIdPartida} setIdJugador={mockedSets} />);
-    const alias =  screen.getByPlaceholderText("Player2") as HTMLInputElement;
-    fireEvent.change(alias, {target: {value: "Torval"}});
+    render(<MockJoinRoom/>);
+    const alias = screen.getByPlaceholderText("Player2") as HTMLInputElement;
+    fireEvent.change(alias, { target: { value: "Torval" } });
     expect(alias.value).toBe("Torval");
 });
 
 test('limite de cantidad de letras alias jugador', () => {
-    render(<FormularioUnirsePartida idPartida={mockIdPartida} setIdJugador={mockedSets} />);
-    const alias =  screen.getByPlaceholderText("Player2") as HTMLInputElement;
-    fireEvent.change(alias, {target: {value: "abdulajrabinachbinbar"}}); //21 caracteres
+    render(<MockJoinRoom/>);
+    const alias = screen.getByPlaceholderText("Player2") as HTMLInputElement;
+    fireEvent.change(alias, { target: { value: "abdulajrabinachbinbar" } }); //21 caracteres
     expect(alias.value.length).not.toBeGreaterThan(mockMaxLonNombres);
+});
+
+test('no submit sin alias', () => {
+    render(<MockJoinRoom/>);
+    const alias = screen.getByPlaceholderText("Player2");
+    expect(alias).toBeRequired();
 });
