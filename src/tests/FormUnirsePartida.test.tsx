ecb2e5acf6002535051057ef0ff41da0a71cbb2d
Maurillugdar <maurillugdar@gmail.com>
2024-10-07 23:20:50 -0300

    ING-98 Agrego test sobre el alias de unirse

--- src/tests/FormUnirsePartida.test.tsx
+++ src/tests/FormUnirsePartida.test.tsx
@@ -5,9 +5,24 @@ import { vi } from 'vitest';
 
 const mockedSets = vi.fn(); 
 const mockMaxLonNombres = 20;
+const mockIdPartida = 0;
 
 test('render formulario de unirse', () => {
-    render(<FormularioUnirsePartida idPartida={0} setIdJugador={mockedSets}/>);
+    render(<FormularioUnirsePartida idPartida={mockIdPartida} setIdJugador={mockedSets}/>);
     const existe = screen.getByText(/Unirse a Sala:/i); // Veo que se muestre la frase/titulo del form de creacion
     expect(existe).toBeVisible();
+});
+
+test('interactuar con input de alias jugador', () => {
+    render(<FormularioUnirsePartida idPartida={mockIdPartida} setIdJugador={mockedSets} />);
+    const alias =  screen.getByPlaceholderText("Player2") as HTMLInputElement;
+    fireEvent.change(alias, {target: {value: "Torval"}});
+    expect(alias.value).toBe("Torval");
+});
+
+test('limite de cantidad de letras alias jugador', () => {
+    render(<FormularioUnirsePartida idPartida={mockIdPartida} setIdJugador={mockedSets} />);
+    const alias =  screen.getByPlaceholderText("Player2") as HTMLInputElement;
+    fireEvent.change(alias, {target: {value: "abdulajrabinachbinbar"}}); //21 caracteres
+    expect(alias.value.length).not.toBeGreaterThan(mockMaxLonNombres);
 });
