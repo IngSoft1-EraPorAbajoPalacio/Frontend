85f6e94a2b114ee594702d4da0573a474dd951f9
Maurillugdar <maurillugdar@gmail.com>
2024-10-16 20:38:30 -0300

    SCRUM-118 remarcar figura correctamente

--- src/utils/Cartas/figuraSeleccionada.ts
+++ src/utils/Cartas/figuraSeleccionada.ts
@@ -24,7 +24,7 @@ export const handleSeleccionFigura = (coordFichaSelec: Coord, figurasDetectadas:
 
     figurasDetectadas.forEach((fig: Figura) => {
         fig.coordenadas.forEach((coord: Coord) => {
-            let numFichaCajon: number = coord[1] + coord[0]*6;
+            let numFichaCajon: number = coord[1]*6 + coord[0];
             if (figuraSeleccionada === null) { // No hay ninguna ficha perteneciente a una figura seleccionada
                 setMarcadasPorSelec([]);
             } else if (figuraSeleccionada !== null && fig.tipoFig === figuraSeleccionada) // Si selecciono una figura marco unicamente esa
