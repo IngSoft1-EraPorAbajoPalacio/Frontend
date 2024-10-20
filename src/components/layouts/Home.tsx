574f26b7b80f4eece2adbac403e197cf8e1651e5
JulietaStorino <julieta_storino@mi.unc.edu.ar>
2024-10-19 23:25:16 -0300

    SCRUM-83 mas correciones

--- src/components/layouts/Home.tsx
+++ src/components/layouts/Home.tsx
@@ -71,36 +71,6 @@ const Home = () => {
             </div>            
             <div id='unirse'>
                 <BusquedaPartidas busqueda={busqueda} setBusqueda={setBusqueda} minPlayers={minPlayers} maxPlayers={maxPlayers} setMinPlayers= {setMinPlayers} setMaxPlayers= {setMaxPlayers} />
-                <div className="filtrarPartidas">
-                    <input
-                        id='barraBusqueda'
-                        type="text"
-                        placeholder="Buscar partidas..."
-                        value={busqueda}
-                        onChange={(e) => setBusqueda(e.target.value)}
-                    />
-                    <div className="filtarJugs">
-                        <label><strong>Filtrar por cantidad de jugadores</strong></label>
-                        <div className="filtroJug">
-                            <div className="filtroJugItem">
-                                <label>Mín</label>
-                                <div className="controlFiltro">
-                                    <button onClick={() => ajustarMin(-1)}>-</button>
-                                    <span>{minPlayers}</span>
-                                    <button onClick={() => ajustarMin(1)}>+</button>
-                                </div>
-                            </div>
-                            <div className="filtroJugItem">
-                                <label>Máx</label>
-                                <div className="controlFiltro">
-                                    <button onClick={() => ajustarMax(-1)}>-</button>
-                                    <span>{maxPlayers}</span>
-                                    <button onClick={() => ajustarMax(1)}>+</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <ListarPartidas setIdPartida={setIdPartida} partidas={partidasFiltradas} />
             </div>
             <Overlay isOpen={partidaCreada} onClose={() => { setPartidaCreada(!partidaCreada) }}>
