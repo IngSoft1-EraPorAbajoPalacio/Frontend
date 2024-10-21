0c8b33db256e016218cef7291fa653bed673465c
Maurillugdar <maurillugdar@gmail.com>
2024-10-21 02:45:56 -0300

    SCRUM-118 Mensaje modificado HTTP

--- src/utils/Cartas/figuraSeleccionada.ts
+++ src/utils/Cartas/figuraSeleccionada.ts
@@ -1,20 +1,21 @@
+import axios from "axios";
 import { Coord, Figura } from "../../types/figura";
 import definirFigMarcadas from "./DefinirFigMarcadas";
 
 
 export const handleSeleccionFigura = (coordFichaSelec: Coord, figurasDetectadas: Figura[],
-    setFiguraSeleccionada:React.Dispatch<React.SetStateAction<number | null>>,
+    setFiguraSeleccionada: React.Dispatch<React.SetStateAction<number | null>>,
     setMarcaFiguras: React.Dispatch<React.SetStateAction<number[]>>,
     setMarcadasPorSelec: React.Dispatch<React.SetStateAction<number[]>>,
-    cartaFiguraDescarte: string |null,
-    gameId: string|undefined,
-    playerId: string|undefined
+    cartaFiguraDescarte: string | null,
+    gameId: string | undefined,
+    playerId: string | undefined
 ) => {
     const { marcarFicha, limpiarFigMarcadas } = definirFigMarcadas(setMarcaFiguras);
-    let figuraSeleccionadaLocal :number|null = null;
-    let fichasDeSeleccionLocal: number[] =[];
-    let fichasParaJuan : Coord[] = [];
-
+    let figuraSeleccionadaLocal: number | null = null;
+    let fichasDeSeleccionLocal: number[] = [];
+    let fichasParaJuan: Coord[] = [];
+    let figuraGuardadaParaJuan: number = 0;
 
     for (let figura of figurasDetectadas) {
         for (let coordenadas of figura.coordenadas) {
@@ -29,7 +30,7 @@ export const handleSeleccionFigura = (coordFichaSelec: Coord, figurasDetectadas:
     setMarcadasPorSelec([]); // Para que no se vayan acumulando las marcadas cada vez que se selecciona
     figurasDetectadas.forEach((fig: Figura) => {
         fig.coordenadas.forEach((coord: Coord) => {
-            let numFichaCajon: number = coord[1]*6 + coord[0];
+            let numFichaCajon: number = coord[1] * 6 + coord[0];
             if (figuraSeleccionadaLocal === null) { // No hay ninguna ficha perteneciente a una figura seleccionada
                 limpiarFigMarcadas([]);
             } else if (figuraSeleccionadaLocal !== null && fig.idFig === figuraSeleccionadaLocal) // Si selecciono una figura marco unicamente esa
@@ -38,37 +39,33 @@ export const handleSeleccionFigura = (coordFichaSelec: Coord, figurasDetectadas:
                 fichasParaJuan.push(coord);
                 marcarFicha(numFichaCajon);
                 limpiarFigMarcadas(fichasDeSeleccionLocal);
+                figuraGuardadaParaJuan = fig.tipoFig;
             }
         })
     });
     setMarcadasPorSelec(fichasDeSeleccionLocal); // Marco las fichas cajon de la seleccionada
 
-
-    const data = {
-        idCarta: Number(cartaFiguraDescarte),
-        fichas: fichasParaJuan
-    };
-
-    const options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-    };
-
+    let idPartida = gameId;
+    let idJugador = playerId;
     const asyncPost = async () => {
         try {
-            
-            const response = 
-                await fetch('http://127.0.0.1:8000/partida/' + Number(gameId) + "/jugador/" + Number(playerId) + "/tablero/declarar-figura", options);
-            if (response.status === 202) {
-                console.log("EPICO")
-            } else {
-                throw new Error('Hubo un problema tratando de enviar figura a descartar.');
-            }
+            console.log(`URL: http://127.0.0.1:8000/partida/${idPartida}/jugador/${idJugador}/tablero/declarar-figura`);
+            const url = `http://127.0.0.1:8000/partida/${(idPartida)}/jugador/${(idJugador)}/tablero/declarar-figura`;
+            const data = {
+                idCarta: Number(cartaFiguraDescarte),
+                fichas: fichasParaJuan,
+                tipo_figura: figuraGuardadaParaJuan
+            };
+            console.log(data);
+            const response = await axios.post(url, data);
+            if ((response.status !== 202)) throw new Error("Hubo un problema tratando de jugando figura.");
         } catch (error) {
             console.error(error);
         }
     };
 
     asyncPost();
+
+
+
 };
