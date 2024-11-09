40c9798aa12f1f42e47ed485b61c89a78b45468f
Maurillugdar <maurillugdar@gmail.com>
2024-11-09 18:21:19 -0300

    SCRUM-188 refactor

--- src/components/context/GameContext.tsx
+++ src/components/context/GameContext.tsx
@@ -208,7 +208,7 @@ export const borrarColorProhibido = () => {
 }
 
 
-// Borra el session storege completo
+// Borra el session storage completo
 export const borrarPartida = () => {
     sessionStorage.clear();
 }
