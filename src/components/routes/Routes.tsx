87e068f3f42e2b10e8c0415e515613abc08b60aa
Maurillugdar <maurillugdar@gmail.com>
2024-11-11 04:07:14 -0300

    SRUM-169 Volver al commit 473781e

--- src/components/routes/Routes.tsx
+++ src/components/routes/Routes.tsx
@@ -1,20 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import { routerType } from "../../types/routes.types";
 import pagesData from "./PagesData";
-import { CartasProvider } from "../utils/Game/CartasBloqueadas";
 
 const Router = () => {
   const pageRoutes = pagesData.map(({ path, title, element }: routerType) => {
-    //Si estamos en el game utilizamos el contexto de las cartas
-    const wrappedElement = title === 'Game' ? (
-      <CartasProvider>
-        {element}
-        </CartasProvider>
-    ) : (
-      element
-    );
-
-    return <Route key={title} path={`/${path}`} element={wrappedElement} />;
+    return <Route key={title} path={`/${path}`} element={element} />;
   });
 
   return <Routes>{pageRoutes}</Routes>;
