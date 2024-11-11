8e0e9daf826fdb6895d1e42322694e785b2f8bfc
Maurillugdar <maurillugdar@gmail.com>
2024-11-11 03:53:27 -0300

    SCRUM-166 envuelvo con cartasProvider solo el Game

--- src/components/routes/Routes.tsx
+++ src/components/routes/Routes.tsx
@@ -1,10 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import { routerType } from "../../types/routes.types";
 import pagesData from "./PagesData";
+import { CartasProvider } from "../utils/Game/CartasBloqueadas";
 
 const Router = () => {
   const pageRoutes = pagesData.map(({ path, title, element }: routerType) => {
-    return <Route key={title} path={`/${path}`} element={element} />;
+    //Si estamos en el game utilizamos el contexto de las cartas
+    const wrappedElement = title === 'Game' ? (
+      <CartasProvider>
+        {element}
+        </CartasProvider>
+    ) : (
+      element
+    );
+
+    return <Route key={title} path={`/${path}`} element={wrappedElement} />;
   });
 
   return <Routes>{pageRoutes}</Routes>;
